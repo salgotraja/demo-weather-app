93d44543b0ea903ba7bc0a4bcd7b721ac1e4c620
Jagdish Salgotra <jagdishsal@gmail.com>
2019-09-11 20:10:48 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,107 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geoCode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const log = console.log
+
+//log(__dirname)
+//log(path.join(__dirname, '../public'))
+
+const app = express()
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engines and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'smac.ai'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About My Family',
+        name: 'smac.ai'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Weather help and FAQs',
+        title: 'Help',
+        name: 'smac.ai'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address.'
+        })
+    }
+
+    geoCode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if(error){
+            return res.send({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location, 
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term.'
+        })
+    }
+
+    log(req.query)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'smac.ai',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'smac.ai',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    log('Server is up on port 3000.')
+})
